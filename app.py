4526d6e78c4606ea7a5ed7b508731f946511cf34
TheRiseOfDavid <david53133@gmail.com>
2020-06-05 21:09:23 +0800

    20200605.2109

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
     user_id = event.source.user_id
-line_bot_api.reply_message(
+    line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=user_id))
 
