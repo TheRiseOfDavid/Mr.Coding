8fbce0ab605bc87a8a3039c6a656d00bec512dfb
TheRiseOfDavid <david53133@gmail.com>
2020-06-05 18:54:32 +0800

    20200605.1854

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from linebot.exceptions import(
 from linebot.models import(
     MessageEvent , TextMessage , TextSendMessage , ImageSendMessage
 )
-
+    
 app = Flask(__name__)
 
 line_bot_api = LineBotApi("78A2zLim1iS0CaUX8VDViSXzPmz2DKN6/eAdapVFXMpFXwtVRjF1K31uLeiVIQU4suIt0irP/yxLcCgbvtEOkzWXuE6IxrglZCLoA0v+YrjdLdnUuRHe9pCEz1/GFZn1K6N2lNUxt+TL+HtSJJfosQdB04t89/1O/w1cDnyilFU=")
@@ -29,11 +29,18 @@ def callback():
 
   return 'OK'
   
-@handler.add(MessageEvent , message= TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-  line_bot_api.reply_message(
-      event.reply_token,
-      TextSendMessage(text=event.message.text))
+    input_text = event.message.text
+
+    if input_text == '@查詢匯率':
+        resp = requests.get('https://tw.rter.info/capi.php')
+        currency_data = resp.json()
+        usd_to_twd = currency_data['USDTWD']['Exrate']
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
   
 if (__name__ == "__main__"):
     app.run()
